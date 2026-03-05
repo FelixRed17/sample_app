1575b8d7f32dc13c938db3453758376478936fab
FelixRed17 <felix.mandyme126@gmail.com>
2026-03-05 16:11:03 +0200

    added login functionality

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,5 @@
 //= link_directory ../stylesheets .scss
 //= link application.js
 //= link_tree ../javascript/controllers//= link_tree ../builds
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
